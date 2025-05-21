d56f332598e6630cb5a8d87a8ba2363268f54b4c
Ahmet Hadžiahmetović <info@pobjeda.com>
2025-05-21 14:21:09 +0200

    dashboad

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');});
 
-/* Route::get('/dashboard', function () {
+ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard'); */
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
