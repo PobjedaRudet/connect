8a1244bb0f0b7a3074e1a5a0c4d51200f888f8c8
Ahmet Hadžiahmetović <info@pobjeda.com>
2025-06-02 14:14:33 +0200

    Prva radna verzija - 04

--- resources/js/Pages/Pregledi/ZaSedamDana.vue
+++ resources/js/Pages/Pregledi/ZaSedamDana.vue
@@ -6,6 +6,7 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead>
                         <tr class="bg-gray-100">
+                            <th class="px-4 py-3"></th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">#</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Zaposleni</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Pozicija</th>
@@ -13,7 +14,20 @@
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-100">
-                        <tr v-for="(item, index) in upcoming" :key="'upcoming-' + index" class="hover:bg-gray-50 transition">
+                        <tr
+                            v-for="(item, index) in upcoming"
+                            :key="'upcoming-' + index"
+                            :class="{'bg-blue-100': selectedUpcoming.includes(index), 'hover:bg-gray-50': !selectedUpcoming.includes(index)}"
+                            class="transition"
+                        >
+                            <td class="px-4 py-4 text-center">
+                                <input
+                                    type="checkbox"
+                                    :checked="selectedUpcoming.includes(index)"
+                                    @change="toggleUpcoming(index)"
+                                    class="form-checkbox h-4 w-4 text-blue-600"
+                                />
+                            </td>
                             <td class="px-6 py-4 text-center">{{ index + 1 }}</td>
                             <td class="px-6 py-4">{{ item.employee.firstName }} {{ item.employee.lastName }}</td>
                             <td class="px-6 py-4">{{ item.employee.radno_mjesto }}</td>
@@ -28,6 +42,7 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead>
                         <tr class="bg-red-50">
+                            <th class="px-4 py-3"></th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">#</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Zaposleni</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Pozicija</th>
@@ -35,7 +50,20 @@
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-100">
-                        <tr v-for="(item, index) in expired" :key="'expired-' + index" class="hover:bg-red-50 transition">
+                        <tr
+                            v-for="(item, index) in expired"
+                            :key="'expired-' + index"
+                            :class="{'bg-red-100': selectedExpired.includes(index), 'hover:bg-red-50': !selectedExpired.includes(index)}"
+                            class="transition"
+                        >
+                            <td class="px-4 py-4 text-center">
+                                <input
+                                    type="checkbox"
+                                    :checked="selectedExpired.includes(index)"
+                                    @change="toggleExpired(index)"
+                                    class="form-checkbox h-4 w-4 text-red-600"
+                                />
+                            </td>
                             <td class="px-6 py-4 text-center">{{ index + 1 }}</td>
                             <td class="px-6 py-4">{{ item.employee.firstName }} {{ item.employee.lastName }}</td>
                             <td class="px-6 py-4">{{ item.employee.radno_mjesto }}</td>
@@ -49,10 +77,33 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
   upcoming: Array,
   expired: Array
 });
+
+const selectedUpcoming = ref([]);
+const selectedExpired = ref([]);
+
+const toggleUpcoming = (index) => {
+  const pos = selectedUpcoming.value.indexOf(index);
+  if (pos === -1) {
+    selectedUpcoming.value.push(index);
+  } else {
+    selectedUpcoming.value.splice(pos, 1);
+  }
+};
+
+const toggleExpired = (index) => {
+  const pos = selectedExpired.value.indexOf(index);
+  if (pos === -1) {
+    selectedExpired.value.push(index);
+  } else {
+    selectedExpired.value.splice(pos, 1);
+  }
+};
 
 const formatDate = (dateStr) => {
   const date = new Date(dateStr);
