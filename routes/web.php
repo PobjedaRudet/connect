2c0e45303df56252c091d5b740b9b57e328d274e
Ahmet Hadžiahmetović <info@pobjeda.com>
2026-07-21 10:05:23 +0200

    Šef Komercijale prava

--- routes/web.php
+++ routes/web.php
@@ -222,7 +222,8 @@ Route::middleware('auth')->group(function () {
 // ║  6. RESURSI, IZLAZNICE, HR                                        ║
 // ╚═══════════════════════════════════════════════════════════════════════╝
 
-Route::middleware('auth', 'adminOrFunkcije:HR,Šef HR,IT,Radnik, Šef PPZ,Šef finansija,Šef službe za SZOI')->group(function () {
+Route::middleware('auth', 'adminOrFunkcije:HR,Šef HR,IT,Radnik, Šef PPZ,Šef finansija,Šef službe za SZOI,Šef Komercijale
+')->group(function () {
 
     // ── Profil ──
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -248,7 +249,8 @@ Route::middleware('auth', 'adminOrFunkcije:HR,Šef HR,IT,Radnik, Šef PPZ,Šef f
     // ┌───────────────────────────────────────────────────────────────────┐
     // │  HR — samo HR korisnik ili admin                                │
     // └───────────────────────────────────────────────────────────────────┘
-    Route::middleware('adminOrFunkcije:HR, Šef HR, IT, Šef PPZ,Šef finansija,Šef službe za SZOI')->group(function () {
+    Route::middleware('adminOrFunkcije:HR, Šef HR, IT, Šef PPZ,Šef finansija,Šef službe za SZOI,Šef Komercijale
+')->group(function () {
 
         Route::get('/sector/hr', function () {
             return Inertia::render('Sector/Hr');
