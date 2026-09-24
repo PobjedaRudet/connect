0ab585ed8dae2fe9c81b787cb3ab28e6d7a5ad29
Ahmet Hadžiahmetović <info@pobjeda.com>
2026-09-24 14:35:13 +0200

    Dodan modul kapija

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ asset('favicon.ico') }}?v=connect" sizes="32x32">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}?v=connect" sizes="32x32">
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=connect">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
